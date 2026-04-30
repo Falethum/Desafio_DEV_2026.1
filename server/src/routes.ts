7add7bdaa8034f01fe86ef4627447ba7fbc53dc5
Falethum <phquixada@gmail.com>
2026-04-30 16:35:49 -0300

    Add files via upload

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,17 @@
 import express from "express";
-import {  readAllUsers } from "./controllers/UserController";
-
-
+import { createUser, deleteUser, readAllUsers, updateUser } from "./controllers/UserController";
+import { createCalcado, deleteCalcado, readAllCalcado, updateCalcado } from "./controllers/CalcadoController";
+ 
 const routes = express.Router();
 
+routes.post("/users", createUser);
 routes.get("/users", readAllUsers);
+routes.patch("/users/:id", updateUser);
+routes.delete("/users/:id", deleteUser);
 
+routes.post("/calcado", createCalcado);
+routes.get("/calcado", readAllCalcado);
+routes.patch("/calcado/:id", updateCalcado);
+routes.delete("/calcado/:id", deleteCalcado);
 
 export default routes;
